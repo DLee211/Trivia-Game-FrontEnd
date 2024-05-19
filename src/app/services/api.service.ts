6adb9ce47ae53ef59ca5e1f548cf5151b4acae39
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-05-19 19:32:14 -0400

    Added Service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  postGame(data: any) {
+    return this.http.post<any>('http://localhost:5249/Game/', data)
+  }
+
+  getGame() {
+    return this.http.get<any>('http://localhost:5249/Game/')
+  }
+
+  updateGame(data: any, id: number) {
+    return this.http.put<any>('http://localhost:5249/Game/' + id, data)
+  }
+
+  deleteGame(id: number) {
+    return this.http.delete<any>('http://localhost:5249/Game/' + id)
+  }
+
+  postQuiz(data: any) {
+    return this.http.post<any>('http://localhost:5249/Quiz/', data)
+  }
+
+  getQuiz() {
+    return this.http.get<any>('http://localhost:5249/Quiz/')
+  }
+
+  updateQuiz(data: any, id: number) {
+    return this.http.put<any>('http://localhost:5249/Quiz/' + id, data)
+  }
+
+  deleteQuiz(id: number) {
+    return this.http.delete<any>('http://localhost:5249/Quiz/' + id)
+  }
+
+  postQuestion(data: any) {
+    return this.http.post<any>('http://localhost:5249/Question/', data)
+  }
+
+  getQuestion() {
+    return this.http.get<any>('http://localhost:5249/Question/')
+  }
+
+  updateQuestion(data: any, id: number) {
+    return this.http.put<any>('http://localhost:5249/Question/' + id, data)
+  }
+
+  deleteQuestion(id: number) {
+    return this.http.delete<any>('http://localhost:5249/Question/' + id)
+  }
 }
