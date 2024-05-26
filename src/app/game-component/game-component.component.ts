f65c7861acaf6914dcd2e65d1afee278160875ab
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-05-25 22:44:38 -0400

    Adding methods to game-component.component.ts, trying to get all games on this page

--- src/app/game-component/game-component.component.ts
+++ src/app/game-component/game-component.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {ApiService} from "../services/api.service";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-game-component',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrl: './game-component.component.css'
 })
 export class GameComponentComponent {
+
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private api : ApiService)
+  {
+  }
+  GetAllSleepData()
+  {
+    this.api.getGame().subscribe({
+      next: (res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
+  }
+
 
 }
