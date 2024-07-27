425674e9c4ba220896e96b962544b88dde1871fa
DLee211 <daniel.lee1921@gmail.com>
2024-07-27 15:40:53 -0400

    Added Validation for the edit game name dialog box

--- src/app/edit-game-dialog/edit-game-dialog.component.ts
+++ src/app/edit-game-dialog/edit-game-dialog.component.ts
@@ -1,21 +1,26 @@
-import { Component, Inject } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormGroup, FormControl } from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {ApiService} from "../services/api.service";
 
 @Component({
   selector: 'app-edit-game-dialog',
   templateUrl: './edit-game-dialog.component.html',
 })
-export class EditGameDialogComponent {
-  editGameForm = new FormGroup({
-    gameType: new FormControl( { value: '', disabled: false }),
-  });
+
+export class EditGameDialogComponent implements OnInit {
+  editGameForm!: FormGroup;
 
   constructor(
     private api: ApiService,
     public dialogRef: MatDialogRef<EditGameDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) {
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  ngOnInit(): void {
+    this.editGameForm = new FormGroup({
+      gameType: new FormControl('', [Validators.required, Validators.maxLength(30)]),
+    });
   }
 
   onNoClick(): void {
@@ -23,25 +28,28 @@ export class EditGameDialogComponent {
   }
 
   submitForm() {
-    if (this.data.gameId) {
-      this.api.getGameById(this.data.gameId).subscribe({
-        next: (gameDetails) => {
-          gameDetails.gameType = this.editGameForm.value.gameType;
-          this.api.updateGame(gameDetails, this.data.gameId).subscribe({
-            next: (res) =>
-              console.log('Response:', res),
-            error: (err) => {
-              console.log('Error:', err);
-              console.log('Body:', gameDetails);
-              console.log('Game ID:', this.data.gameId);
-            }
-          });
-          location.reload()
-        },
-        error: (err) => console.error('Failed to fetch game details', err)
-      });
+    if (this.editGameForm.valid) {
+      if (this.data.gameId) {
+        this.api.getGameById(this.data.gameId).subscribe({
+          next: (gameDetails) => {
+            gameDetails.gameType = this.editGameForm.value.gameType;
+            this.api.updateGame(gameDetails, this.data.gameId).subscribe({
+              next: (res) => console.log('Response:', res),
+              error: (err) => {
+                console.log('Error:', err);
+                console.log('Body:', gameDetails);
+                console.log('Game ID:', this.data.gameId);
+              }
+            });
+            location.reload();
+          },
+          error: (err) => console.error('Failed to fetch game details', err)
+        });
+      } else {
+        console.error('Game ID is missing');
+      }
     } else {
-      console.error('Game ID is missing');
+      console.error('Form is invalid');
     }
   }
 }
