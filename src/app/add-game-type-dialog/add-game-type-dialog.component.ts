2399164b6db14f26d4bc0c6ee3522a87c3f31d20
DLee211 <daniel.lee1921@gmail.com>
2024-07-27 15:52:17 -0400

    Added add game validation

--- src/app/add-game-type-dialog/add-game-type-dialog.component.ts
+++ src/app/add-game-type-dialog/add-game-type-dialog.component.ts
@@ -15,29 +15,31 @@ export class AddGameTypeDialogComponent {
   constructor(private apiService: ApiService,private router: Router,     public dialogRef: MatDialogRef<AddGameTypeDialogComponent>
   ) {
     this.gameTypeForm = new FormGroup({
-      gameType: new FormControl('', Validators.required)
+      gameType: new FormControl('', [Validators.required, Validators.maxLength(30)])
     });
   }
 
 
   addGameType() {
-    const requestBody = {
-      gameId: 0,
-      gameType: this.gameTypeForm.value.gameType,
-      score: 0
-    };
-    console.log('Sending request with body:', requestBody);
-    this.apiService.postGame(requestBody).subscribe({
-      next: (res) => {
-        console.log('Response:', res);
-        alert("Game type added successfully");
-        this.gameTypeForm.reset();
-        this.dialogRef.close();
-        this.router.navigate(['/game']);
-      },
-      error: (err) => {
-        console.log('Error:', err);
-      }
-    });
+    if (this.gameTypeForm.valid) {
+      const requestBody = {
+        gameId: 0,
+        gameType: this.gameTypeForm.value.gameType,
+        score: 0
+      };
+      console.log('Sending request with body:', requestBody);
+      this.apiService.postGame(requestBody).subscribe({
+        next: (res) => {
+          console.log('Response:', res);
+          alert("Game type added successfully");
+          this.gameTypeForm.reset();
+          this.dialogRef.close();
+          this.router.navigate(['/game']);
+        },
+        error: (err) => {
+          console.log('Error:', err);
+        }
+      });
+    }
   }
 }
